3be30b842489a43e9b55f5b29cc69cce8038cb8d
fschwanck <fschwanck@gmail.com>
2023-07-09 23:34:05 -0300

    Adicionado código para tabela 3

---
+++ sql/tabela_3.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE IF NOT EXISTS `fschwanck-case-gb-2`.`refined`.`tabela_3`(
+    MARCA STRING,
+    ANO STRING,
+    MES STRING,
+    QTD_VENDA INTEGER
+
+);
+TRUNCATE TABLE `fschwanck-case-gb-2`.`refined`.`tabela_3`;
+INSERT `fschwanck-case-gb-2`.`refined`.`tabela_3` (MARCA, ANO, MES, QTD_VENDA)
+SELECT 
+  MARCA,
+  format_date('%Y', DATA_VENDA) AS ANO,
+  format_date('%m', DATA_VENDA) AS MES,
+  SUM(QTD_VENDA) AS QTD_VENDA
+FROM `fschwanck-case-gb-2`.`raw`.`tabela_base`
+GROUP BY MARCA, ANO, MES
